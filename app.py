ade4386629a9b09abb0cf284dd9322f6477ea148
aboud20-meet <aboud20@meet.mit.edu>
2018-11-15 18:49:48 +0200

    flaskss

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask
-from flask import render_template
-from database import get_all_cats, get_cat_by_id
+from flask import render_template , request, redirect
+from database import get_all_cats, get_cat_by_id,create_cat
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'YOUR-VERY-SECRET-SHHH'
@@ -14,6 +14,15 @@ def catbook_home():
 def cat_page(id):
 	cats = get_cat_by_id(id)
 	return render_template("cat.html", cat=cats)
+
+@app.route('/add_cat', methods=["GET","POST"])
+def create_cats():
+	if request.method == 'GET':
+		return render_template("add_cat.html")
+	else:
+		name = request.form["name"]
+		create_cat(name)
+		return redirect("/")
 
 if __name__ == '__main__':
    app.run(debug = True)
